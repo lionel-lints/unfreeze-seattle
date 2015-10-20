c88b8b16f29c0ddaf8ab34ffac6a822885bc2d9d
Mary Mott <mott.maryt@gmail.com>
2015-10-20 13:39:21 -0700

    jquery loading shades

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,15 +4,30 @@ $(function() {
 
   // this function shows map and adds a new place
   function initialize() {
+    var fileName = ['admiral', 'alki', 'arbor_heights', 'atlantic', 'ballard'];
+    // var geojson;
     //load and center map on US
     map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 12,
+      zoom: 11,
       center: {lat: 47.6062095, lng: -122.3320708}  //center it on united states
     });
 
-    var mapDiv = document.getElementById('map-canvas');
+    // geojson = JSON.parse('geojson/ballard.geojson');
 
-    map.data.loadGeoJson('/geojson/ballard.geojson');
+    fileName.forEach(function(neighborhood) {
+      // console.log(neighborhood);
+      map.data.loadGeoJson('geojson/' + neighborhood + '.geojson');
+
+    });
+    // map.data.loadGeoJson('geojson/admiral.geojson');
+    // map.data.loadGeoJson('geojson/alki.geojson');
+    // map.data.loadGeoJson('geojson/ballard.geojson');
+
+
+    map.data.setStyle({
+      fillColor: 'purple',
+      strokeWeight: 1
+    });
 
     // google.maps.event.addListener(map, 'click', addMarker);
   }
