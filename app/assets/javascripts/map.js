7d7a6432510c8e9399f358766990ba99d882c8c6
Mary Mott <mott.maryt@gmail.com>
2015-10-19 22:27:26 -0700

    map of seattle centered on index page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,17 +2,30 @@ $(function() {
   var map;
   var $body = $('body');
 
-  //this function shows map and adds a new place
-  (function initialize() {
+  // this function shows map and adds a new place
+  function initialize() {
     //load and center map on US
     map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 4,
-      center: {lat: 39.5, lng: -98.35}  //center it on united states
+      zoom: 12,
+      center: {lat: 47.6062095, lng: -122.3320708}  //center it on united states
     });
 
     var mapDiv = document.getElementById('map-canvas');
 
     // google.maps.event.addListener(map, 'click', addMarker);
-  })();
+  }
+
+
+  // function initialize() {
+  //   var mapCanvas = document.getElementById('map-canvas');
+  //   var mapOptions = {
+  //     center: new google.maps.LatLng(39.5, -98.35),
+  //     zoom: 4,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   }
+  //   var map = new google.maps.Map(mapCanvas, mapOptions)
+  // }
+
+  initialize();
 
 });
