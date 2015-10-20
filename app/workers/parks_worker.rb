f32cd799327cbf16a294b4b9d91b9206ea2e6c5e
RSR312-206 <roccosummer@gmail.com>
2015-10-20 13:46:05 -0700

    added gems and files

---
+++ app/workers/parks_worker.rb
@@ -0,0 +1,17 @@
+#NW Quadrant
+
+within_box(location, 47.74448918324826, -122.43232727050781, 47.601579435823446, -122.3127007484436) AND city_feature='parks'
+
+#NE Quadrant
+
+#within_box(location, 47.7352538334833, -122.33345031738281, 47.6423475920095, -122.24281311035156) AND city_feature='parks'
+
+#SW Qudatrant
+
+within_box(location, 47.608752618414165, -122.42374420166016, 47.49521196622769, -122.32349395751953 ) AND city_feature='parks'
+
+#SE Quadrant
+
+#within_box(location, 47.652524500386484, -122.33551025390625, 47.504301372234416, -122.24349975585938) AND city_feature='parks'
+
+
