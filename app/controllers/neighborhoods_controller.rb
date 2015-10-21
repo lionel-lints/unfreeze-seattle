d225d3d880c4047020098b91a3fd3c2529c336ed
RSR312-206 <roccosummer@gmail.com>
2015-10-21 16:44:20 -0700

    building out controllers and views

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,11 +1,11 @@
 class NeighborhoodsController < ApplicationController
+
   def index
   end
 
   def show
-
+   @neighborhood = Neighborhood.find_by(name: params[:neighborhood])
+   @wiki = Wil
   end
-
-  private
 
 end
