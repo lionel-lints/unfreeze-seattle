02cce529f39b093ecf7ad89b9d7dee9d5227f6cd
Dmitry Gempler <DGempler@gmail.com>
2015-10-21 07:49:08 -0700

    added a neighborhood_seed rake file, updated temp files

---
+++ temp/neighborhoods_array.rb
@@ -0,0 +1,16 @@
+neighborhoods =
+ ['admiral', 'alki', 'NW', 'arbor_heights', 'atlantic', 'ballard', 'beacon_hill', 'belltown',
+  'bitter_lake', 'blue_ridge', 'brighton', 'broadmoor', 'broadview', 'bryant',
+  'capitol_hill', 'cedar_park', 'central_district', 'columbia_city', 'crown_hill',
+  'denny', 'downtown', 'eastlake', 'fauntleroy', 'first_hill', 'fremont', 'georgetown',
+  'green_lake', 'greenwood', 'haller_lake', 'hawthorne_hills', 'high_point',
+  'highland_park', 'industrial_district', 'interbay', 'international_district',
+  'laurelhurst', 'leschi', 'lower_queen_anne', 'loyal_heights', 'madison_park',
+  'madison_valley', 'madrona', 'magnolia', 'maple_leaf', 'matthews_beach', 'meadowbrook',
+  'montlake', 'mount_baker', 'north_beach', 'north_college_park', 'north_delridge',
+  'northgate', 'olympic_hills', 'olympic_manor', 'phinney_ridge', 'pinehurst',
+  'pioneer_square', 'portage_bay', 'queen_anne', 'rainier_beach', 'ravenna', 'riverview',
+  'roosevelt', 'roxhill', 'sand_point', 'seward_park', 'south_delridge',
+  'south_lake_union', 'south_park', 'sunset_hill', 'university_district',
+  'victory_heights', 'view_ridge', 'wallingford', 'wedgwood', 'west_seattle', 'westlake',
+  'whittier_heights', 'windermere']
