436accdd327474051d26e70a726d10929f7fc0ea
Dmitry Gempler <DGempler@gmail.com>
2015-10-21 18:25:33 -0700

    finished wiki worker, assuming api call is being done right

--- app/workers/wikis_worker.rb
+++ app/workers/wikis_worker.rb
@@ -1,27 +1,10 @@
 class WikisWorker
   include Sidekiq::Worker
-
-  def perform
-    neighborhoods = Neighborhood.all
-    neighborhoods.each do |hood|
-      Typhoeus.get(hood.wiki_url, followlocation: true)
-      if hood.wiki_blurb !== request.response.body.extract
-        hood.update_attribute(wiki_blurb, request.response.body.extract)
-      end
+    def perform(hood_id)
+      hood = Neighborhood.find(hood_id)
+      response = Typhoeus.get(hood.wiki_url)
+      wiki = Wiki.create(data: response.body)
+      hood.wikis << wiki
+      hood.save
     end
-  end
 end
-
-
-
-  def perform(hood_id)
-    hood = Neighborhood.find(hood_id)
-    response = Typhoeus.get(hood.wiki_url)
-    museums_array = JSON.parse(response.body)
-    unless museums_array.size == 0
-      museums_array.each do |m|
-        museum = Museum.create(data: JSON.generate(m))
-        hood.museums << museum
-        hood.save
-      end
-    end
