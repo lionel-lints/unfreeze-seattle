e8ab9ff8b62fcd8e4137881fa0dc3c235c24080a
Dmitry Gempler <DGempler@gmail.com>
2015-10-21 18:16:54 -0700

    working on wikis_worker, getting nothing back from wikipedia, waiting for Lionel's response

--- app/workers/wikis_worker.rb
+++ app/workers/wikis_worker.rb
@@ -11,3 +11,17 @@ class WikisWorker
     end
   end
 end
+
+
+
+  def perform(hood_id)
+    hood = Neighborhood.find(hood_id)
+    response = Typhoeus.get(hood.wiki_url)
+    museums_array = JSON.parse(response.body)
+    unless museums_array.size == 0
+      museums_array.each do |m|
+        museum = Museum.create(data: m)
+        hood.museums << museum
+        hood.save
+      end
+    end
