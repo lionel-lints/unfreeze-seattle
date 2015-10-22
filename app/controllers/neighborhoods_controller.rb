4c16b18b9550f9501e94a61929e95bd5e7e51c51
RSR312-206 <roccosummer@gmail.com>
2015-10-22 11:43:23 -0700

    show and controllers work

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -6,36 +6,15 @@ class NeighborhoodsController < ApplicationController
 
   def show
    @neighborhood = Neighborhood.find_by(name: params[:neighborhood])
-   response = Typhoeus.get(@neighborhood.wiki_url)
-   data = JSON.parse(response.body)
-   data['query']['pages'].each do|key, value|
-    @wiki = value['extract']
-  end
-
+   data = @neighborhood.wikis[0].data
+    if data != ""
+      data = JSON.parse(data)
+      data['query']['pages'].each do|key, value|
+      @wiki = value['extract']
+      end
+    else
+      @wiki = "Olympic Manor is a gated community in Seattle."
+    end
   end
 
 end
-
-
-#Lionel and Rachel to build out Wiki show
-#then start working on the show map
-
-
-# def perform(hood_id)
-#      hood = Neighborhood.find(hood_id)
-#      response = Typhoeus.get(hood.wiki_url)
-#      wiki = Wiki.create(data: response.body)
-#      hood.wikis << wiki
-#      hood.save
-#      puts "wiki: #{wiki.id}"
-#    end
-
-# supplies = []
-
-# holiday_supplies.each do |key, value|
-#   value.each do |key2, value2|
-#     supplies << value2
-#   end
-# end
-
-# puts supplies.flatten.uniq
