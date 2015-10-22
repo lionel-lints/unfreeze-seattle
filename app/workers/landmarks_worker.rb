3e403e1d88ae0a5c4b1c7e403750fe85eecc9e68
Dmitry Gempler <DGempler@gmail.com>
2015-10-21 17:44:48 -0700

    working on landmarks worker, going to try calling multiple workers from rake command instead

--- app/workers/landmarks_worker.rb
+++ app/workers/landmarks_worker.rb
@@ -5,11 +5,58 @@ class LandmarksWorker
     neighborhoods = Neighborhood.all
     neighborhoods.each do |hood|
       add_on = '&city_feature=Landmarks'
-      app_token = '&$$app_token=' + ENV['SOCRATA_TOKEN']
-      response = Typhoeus.get(hood.seattle_url + add_on + app_token)
-      response_array = response.body
-      p response_array
-      # or response.response_body
+      base_uri = URI.encode(hood.seattle_url)
+      response = Typhoeus::Request.get(
+        url + add_on,
+        headers: { 'X-App-Token' => "BaCM5K6CYxW0AGrn56SOgqmL1" }
+      )
+      #ENV['SOCRATA_TOKEN']
+      landmarks_array = JSON.parse(response.body)
+      unless landmarks_array.size == 0
+        landmarks_array.each do |l|
+          landmark = Landmark.create(data: l)
+          hood.landmarks << landmark
+        end
+      end
     end
   end
 end
+
+
+=begin
+request = Typhoeus::Request.get(
+  url + '&city_feature=Parks',
+  headers: { 'X-App-Token' => "BaCM5K6CYxW0AGrn56SOgqmL1" }
+)
+
+
+Host: soda.demo.socrata.com
+Accept: application/json
+X-App-Token: [REDACTED]
+
+
+
+
+
+'Access-Control-Allow-Origin': *
+'Age': 0
+'Connection': keep-alive
+'Content-Encoding': gzip
+'Content-Type': application/json;charset=utf-8
+'Server': nginx
+'Transfer-Encoding': chunked
+'X-Socrata-Region': production
+'X-Soda2-Data-Out-Of-Date': false
+'X-Soda2-Fields': ["address","city_feature","common_name","featureitem_id","latitude","location","longitude","website"]
+'X-Soda2-Types': ["text","text","text","number","number","point","number","text"]
+'X-App-Token' => "BaCM5K6CYxW0AGrn56SOgqmL1"
+
+
+URI.encode(params[:song])
+
+
+=end
+
+
+
+
