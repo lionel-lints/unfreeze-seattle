6ec2e623939f5af6de236095ddd7a39e4237693c
Dmitry Gempler <DGempler@gmail.com>
2015-10-22 07:43:40 -0700

    deleted commented out code in landmarks worker

--- app/workers/landmarks_worker.rb
+++ app/workers/landmarks_worker.rb
@@ -22,40 +22,4 @@ class LandmarksWorker
 end
 
 
-=begin
-request = Typhoeus::Request.get(
-  url + '&city_feature=Parks',
-  headers: { 'X-App-Token' => "BaCM5K6CYxW0AGrn56SOgqmL1" }
-)
-
-
-Host: soda.demo.socrata.com
-Accept: application/json
-X-App-Token: [REDACTED]
-
-
-
-
-
-'Access-Control-Allow-Origin': *
-'Age': 0
-'Connection': keep-alive
-'Content-Encoding': gzip
-'Content-Type': application/json;charset=utf-8
-'Server': nginx
-'Transfer-Encoding': chunked
-'X-Socrata-Region': production
-'X-Soda2-Data-Out-Of-Date': false
-'X-Soda2-Fields': ["address","city_feature","common_name","featureitem_id","latitude","location","longitude","website"]
-'X-Soda2-Types': ["text","text","text","number","number","point","number","text"]
-'X-App-Token' => "BaCM5K6CYxW0AGrn56SOgqmL1"
-
-
-URI.encode(params[:song])
-
-
-=end
-
-
-
 
