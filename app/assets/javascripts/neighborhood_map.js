a7e2d318f61810415f1b164e38719feedc549248
LionelLints <lionel.lints@gmail.com>
2015-10-22 14:55:06 -0700

    fixed multiple map views

---
+++ app/assets/javascripts/neighborhood_map.js
@@ -0,0 +1,13 @@
+$(function() {
+  var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 47.6062095, lng: -122.3320708}
+    // mapTypeId: google.maps.mapTypeId.TERRAIN
+  });
+}
+console.log("***************************************")
+initMap();
+
+});
