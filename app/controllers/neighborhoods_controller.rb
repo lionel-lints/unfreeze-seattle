d735850a3a8e8967793949bbd80a90f4e6465f07
Mary Mott <mott.maryt@gmail.com>
2015-10-22 11:16:05 -0700

    updated controller

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -5,8 +5,12 @@ class NeighborhoodsController < ApplicationController
   end
 
   def show
-   @neighborhood = Neighborhood.find_by(name: params[:neighborhood])
-   @wiki = Wil
+    @neighborhood = Neighborhood.find_by(name: params[:neighborhood])
+    response = Typhoeus.get(@neighborhood.wiki_url)
+    data = JSON.parse(response.body)
+    data['query']['pages'].each do|key, value|
+      @wiki = value['extract']
+    end
   end
 
 end
