8699f01f6b9d0b5d06423c132898bb0a7299cb06
RSR312-206 <roccosummer@gmail.com>
2015-10-23 10:42:43 -0700

    show fucking works

--- app/assets/javascripts/neighborhood_map.js
+++ app/assets/javascripts/neighborhood_map.js
@@ -3,16 +3,17 @@ $(function() {
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
-    center: {},
+    center: {lat: 47.6062095, lng: -122.3320708},
     mapTypeId: google.maps.MapTypeId.TERRAIN
   });
 }
 file = neighborhood.polygon_url
+initMap();
+
 map.data.loadGeoJson(file);
 
 
 
 
-initMap();
 
 });
