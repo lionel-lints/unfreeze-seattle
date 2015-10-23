476a1783f76f8966bb932b0df4c156de48b7689b
LionelLints <lionel.lints@gmail.com>
2015-10-23 16:51:31 -0700

    fix local redis url, remove redis cache

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = URI.parse(ENV["REDISTOGO_URL"]) || "redis://localhost:3000/"
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:3000/")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
