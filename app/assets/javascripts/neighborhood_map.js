5a95b970eca61329b387abff45b773b5c5eb6af2
RSR312-206 <roccosummer@gmail.com>
2015-10-23 16:59:41 -0700

    working

--- app/assets/javascripts/neighborhood_map.js
+++ app/assets/javascripts/neighborhood_map.js
@@ -1,78 +1,87 @@
 $(function() {
   var map;
+  var marker;
+  var infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     center: {lat: 47.6062095, lng: -122.3320708},
     mapTypeId: google.maps.MapTypeId.TERRAIN
   });
+}
 
-  var marker;
-  var infoWindow;
-  museums.forEach(function(museum, index) {
-    marker = new google.maps.Marker({
-      position: {lat: museum.latlng.lat, lng: museum.latlng.lng},
-      map: map,
-      title: museum.name
-    });
-    marker.addListener('click', function(event) {
-    museum_content = '<h3>MUSEUM</h3><a href=' + '"' + museum.url + '"' + 'target="_blank">' + museum.name + '</a> <br>' + museum.address;
-      if (infoWindow) {
-        infoWindow.close();
-      }
-      infoWindow = new google.maps.InfoWindow({
-        content: museum_content,
-        position: event.latLng
+  function museumMarkers() {
+    museums.forEach(function(museum, index) {
+      marker = new google.maps.Marker({
+        position: {lat: museum.latlng.lat, lng: museum.latlng.lng},
+        map: map,
+        title: museum.name
       });
-      infoWindow.open(map);
+      marker.addListener('click', function(event) {
+      museum_content = '<h3>MUSEUM</h3><a href=' + '"' + museum.url + '"' + 'target="_blank">' + museum.name + '</a> <br>' + museum.address;
+        if (infoWindow) {
+          infoWindow.close();
+        }
+        infoWindow = new google.maps.InfoWindow({
+          content: museum_content,
+          position: event.latLng
+        });
+        infoWindow.open(map);
+      });
+      marker.setMap(map);
     });
-    marker.setMap(map);
-  });
+  }
 
   //landmarks
-  landmarks.forEach(function(landmark, index) {
-    marker = new google.maps.Marker({
-      position: {lat: landmark.latlng.lat, lng: landmark.latlng.lng},
-      map: map,
-      title:landmark.name
-    });
-    marker.addListener('click', function(event) {
-    landmark_content = '<h3>LANDMARK</h3><a href=' + '"' + landmark.url + '"' + 'target="_blank">' + landmark.name + '</a> <br>' + landmark.address;
-      if (infoWindow) {
-        infoWindow.close();
-      }
-      infoWindow = new google.maps.InfoWindow({
-        content: landmark_content,
-        position: event.latLng
+  function landmarkMarkers() {
+    landmarks.forEach(function(landmark, index) {
+      marker = new google.maps.Marker({
+        position: {lat: landmark.latlng.lat, lng: landmark.latlng.lng},
+        map: map,
+        title:landmark.name
       });
-      infoWindow.open(map);
+      marker.addListener('click', function(event) {
+      landmark_content = '<h3>LANDMARK</h3><a href=' + '"' + landmark.url + '"' + 'target="_blank">' + landmark.name + '</a> <br>' + landmark.address;
+        if (infoWindow) {
+          infoWindow.close();
+        }
+        infoWindow = new google.maps.InfoWindow({
+          content: landmark_content,
+          position: event.latLng
+        });
+        infoWindow.open(map);
+      });
+      marker.setMap(map);
     });
-    marker.setMap(map);
-  });
+  }
   // parks
-  parks.forEach(function(park, index) {
-    marker = new google.maps.Marker({
-      position: {lat: park.latlng.lat, lng: park.latlng.lng},
-      map: map,
-      title:park.name
-    });
-    marker.addListener('click', function(event) {
-    park_content = '<h3>PARK</h3><a href=' + '"' + park.url + '"' + 'target="_blank">' + park.name + '</a> <br>' + park.address;
-      if (infoWindow) {
-        infoWindow.close();
-      }
-      infoWindow = new google.maps.InfoWindow({
-        content: park_content,
-        position: event.latLng
+  function parkMarkers() {
+    parks.forEach(function(park, index) {
+      marker = new google.maps.Marker({
+        position: {lat: park.latlng.lat, lng: park.latlng.lng},
+        map: map,
+        title:park.name
       });
-      infoWindow.open(map);
+      marker.addListener('click', function(event) {
+      park_content = '<h3>PARK</h3><a href=' + '"' + park.url + '"' + 'target="_blank">' + park.name + '</a> <br>' + park.address;
+        if (infoWindow) {
+          infoWindow.close();
+        }
+        infoWindow = new google.maps.InfoWindow({
+          content: park_content,
+          position: event.latLng
+        });
+        infoWindow.open(map);
+      });
+      marker.setMap(map);
     });
-    marker.setMap(map);
-  });
-
- }
+  }
 
 initMap();
+museumMarkers();
+parkMarkers();
+landmarkMarkers();
+
 //load the neighborhood polygons
 file = neighborhood.polygon_url
 map.data.loadGeoJson(file);
