e4f883d0c3bdfd8bfa8aed266807eca089b323bb
Dmitry Gempler <DGempler@gmail.com>
2015-10-26 12:42:11 -0700

    added some to-do items to list

---
+++ temp/to-do.rb
@@ -0,0 +1,24 @@
+Write tests (listed in separate test_list file)
+
+CSS
+
+  white space
+
+
+Refactoring
+
+  indentation
+
+
+Rake / DB seeding / Workers
+
+  fix database seeding (missing features)
+
+  Send rake for each statements into separate workers?
+
+  Refactor all workers to just have one worker?
+
+
+
+Misc / Notes
+  rake db:drop; rake db:create; rake db:migrate; rake db:seed:neighborhoods; rake db:seed:parks; rake db:seed:landmarks; rake db:seed:museums; rake db:seed:wikis
